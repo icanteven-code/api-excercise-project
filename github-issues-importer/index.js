72bc0f203e4c550dbc47639227031e1f48665b1f
Simeon Petrov <petrov.v.simeon@gmail.com>
2022-08-18 20:45:54 +0300

    feat: Parse md file and create a ticket from the content

--- github-issues-importer/index.js
+++ github-issues-importer/index.js
@@ -1,26 +1,67 @@
 import * as dotenv from "dotenv";
 import { Octokit } from "octokit";
+import MarkdownIt from 'markdown-it';
+import { readFileSync } from 'fs'
+
+
 dotenv.config({ path: '.env' });
+const md = new MarkdownIt();
+
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_PERSONAL_TOKEN
 })
 
-const createTicket = async () => {
+const createGithubLabel = async () => {
     try {
-        const data = await octokit.request(`POST /repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/issues`, {
+        const existingLabel = await octokit.request(`GET /repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/labels/story`, {
             owner: 'OWNER',
             repo: 'REPO',
-            title: 'Found a bug 2',
-            body: 'I\'m having a problem with this.',
-            labels: [
-                'bug'
-            ]
+            name: 'story'
         })
-        console.log('DATA', data)
+        if (existingLabel.status === 200) return;
+
+        await octokit.request(`POST /repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/labels`, {
+            owner: 'OWNER',
+            repo: 'REPO',
+            name: 'story',
+            description: 'A small part of a feature you need to implement.',
+            color: '27ae60'
+        })
     } catch (e) {
-        console.error('OOPS', e)
+        console.error('Failed to fetch or create custom github label', e)
     }
 }
 
-createTicket();
+const createTicket = async (title, body) => {
+    try {
+        await octokit.request(`POST /repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}/issues`, {
+            owner: 'OWNER',
+            repo: 'REPO',
+            title: title,
+            body: body,
+            labels: [
+                'story'
+            ]
+        })
+    } catch (e) {
+        console.error('Failed to create issue', e)
+    }
+}
+
+const getMarkdownContent = () => {
+    const splitter = '**Description**';
+    const [result] = md.parseInline(file);
+    // const
+    console.log(result);
+    const [title, body] = result.content.split(splitter);
+    return {
+        title: title.replace('#### ', ''),
+        body: splitter + body
+    }
+}
+
+await createGithubLabel();
+const file = readFileSync('../tasks/11-Tags-Filtering-products-by-tags.md', 'utf8')
+const { title, body } = getMarkdownContent(file)
+createTicket(title, body);
